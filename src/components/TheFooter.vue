8084e98c3d985ea8f8864758a956c6174e555208
otossy <otossy.the.best@gmail.com>
2023-08-19 15:46:07 +0900

    とりあえず

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <small>&copy;otossy</small>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.footer {
+  margin-top: 15px;
+  height: 30px;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="title">TODO</h1>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+  .title {
+    width: 100%;
+    background-color: #e3f2fd;
+    text-align: center;
+    font-size: 32px;
+    font-weight: bold;
+    padding: 8px 0;
+  }
+</style>
