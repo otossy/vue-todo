e54aa93d02b4dc7cec8cd450986d6368d0293752
otossy <otossy.the.best@gmail.com>
2023-08-20 22:40:38 +0900

    「追加」ボタンを状態によって「変更」にする対応

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -6,7 +6,19 @@
       class="todo_input"
       placeholder="+ TODOを入力"
     />
-    <button class="btn" @click="addTodo">追加</button>
+    <button class="btn green" @click="editTodo" v-if="isEditRef">変更</button>
+    <button class="btn" @click="addTodo" v-else>追加</button>
+  </div>
+  <div class="box_list">
+    <div class="todo_list" v-for="todo in todoListRef" :key="todo.id">
+      <div class="todo">
+        <input type="checkbox" class="check" /><label>{{ todo.task }}</label>
+      </div>
+      <div class="btns">
+        <button class="btn green" @click="showTodo(todo.id)">編</button>
+        <button class="btn pink">削</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +29,9 @@ export default {
   data: () => {
     return {
       todoRef: '',
-      todoListRef: [],
+      todoListRef: JSON.parse(localStorage.todoList),
+      isEditRef: false,
+      editTodo: () => {}
     };
   },
   methods: {
@@ -27,6 +41,11 @@ export default {
       this.todoListRef.push({id: id, task: this.todoRef});
       localStorage.todoList = JSON.stringify(this.todoListRef);
       this.todoRef = '';
+    },
+    showTodo(id) {
+      const todo = this.todoListRef.find((todo) => todo.id === id);
+      this.todoRef = todo.task;
+      this.isEditRef = true;
     }
   },
 };
@@ -53,5 +72,44 @@ export default {
   color: #fff;
   text-align: center;
   font-size: 14px;
+}
+
+.box_list {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+}
+
+.todo_list {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 12px;
+  width: 300px;
+}
+
+.check {
+  border: 1px solid red;
+  transform: scale(1.6);
+  margin: 0 16px 2px 6px;
+}
+
+.btns {
+  display: flex;
+  gap: 4px;
+}
+
+.green {
+  background-color: #00c853;
+}
+
+.pink {
+  background-color:  #ff4081;
 }
 </style>
